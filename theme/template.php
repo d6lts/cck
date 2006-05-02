3e0ae354653441a36b197a129aa887cb1423b1c9
Jonathan Chaffer <jonbob@831.no-reply.drupal.org>
2006-05-02 14:09:54 +0000

    #55539: CSS classes should use dashes, not underscores.

--- theme/template.php
+++ theme/template.php
@@ -5,6 +5,7 @@ function phptemplate_field(&$node, &$field, &$items, $teaser, $page) {
     'node' => $node,
     'field' => $field,
     'field_type' => $field['type'],
+    'field_name' => $field['field_name'],
     'label' => $field['widget']['label'],
     'items' => $items,
     'teaser' => $teaser,
