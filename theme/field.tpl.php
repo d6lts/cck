ceee3df90c868ddf909212d6617b193eaa853647
Yves Chedemois <yched.drupal@free.fr>
2008-08-27 15:10:35 +0000

    #266205 followup (didn't get included in previous commit)

--- theme/field.tpl.php
+++ theme/field.tpl.php
@@ -4,17 +4,19 @@
     <div class="field-label"><?php print $label ?>:&nbsp;</div>
   <?php endif;?>
   <div class="field-items">
-    <?php foreach ($items as $delta => $item) :
-            if (!empty($item['view']) || $item['view'] === "0") : ?>
-              <div class="field-item">
-                <?php if ($label_display == 'inline') { ?>
-                  <div class="field-label-inline<?php print($delta ? '' : '-first')?>">
-                    <?php print $label ?>:&nbsp;</div>
-                <?php } ?>
-                <?php print $item['view'] ?>
-              </div>
-      <?php endif;
-          endforeach;?>
+    <?php $count = 1;
+      foreach ($items as $delta => $item) :
+        if (!empty($item['view']) || $item['view'] === "0") : ?>
+          <div class="field-item <?php print ($count % 2 ? 'odd' : 'even') ?>">
+            <?php if ($label_display == 'inline') { ?>
+              <div class="field-label-inline<?php print($delta ? '' : '-first')?>">
+                <?php print $label ?>:&nbsp;</div>
+            <?php } ?>
+            <?php print $item['view'] ?>
+          </div>
+        <?php $count++;
+        endif;
+    endforeach;?>
   </div>
 </div>
 <?php endif; ?>
