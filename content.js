d4ca20c47553e27cefa65e5850f2ed4cb4d65ff1
Yves Chedemois <yched.drupal@free.fr>
2008-09-12 23:56:03 +0000

    #234774 followup - try to preserve selected widget when field type changes, even if widget machine name is different (optionwidgets_buttons vs. nodereference_buttons)

--- content.js
+++ content.js
@@ -13,20 +13,22 @@ function attachUpdateSelects(context) {
     this.initialValue = this.options[0].text;
   });
 
-  // Field type select updates its widget select.
+  // 'Field type' select updates its 'Widget' select.
   $('#content-field-overview .content-field-type-select', context).each(function() {
     this.targetSelect = $('.content-widget-type-select', $(this).parents('tr'));
 
     $(this).change(function() {
       var selectedFieldType = this.options[this.selectedIndex].value;
       var options = (selectedFieldType in widgetTypes) ? widgetTypes[selectedFieldType] : [ ];
-      this.targetSelect.contentOptions(options);
+      this.targetSelect.contentPopulateOptions(options);
     });
 
+    // Trigger change on initial pageload to get the right widget options
+    // when field type comes pre-selected (on failed validation).
     $(this).trigger('change');
   });
 
-  // Existing field select updates its widget select and label textfield.
+  // 'Existing field' select updates its 'Widget' select and 'Label' textfield.
   $('#content-field-overview .content-field-select', context).each(function() {
     this.targetSelect = $('.content-widget-type-select', $(this).parents('tr'));
     this.targetTextfield = $('.content-label-textfield', $(this).parents('tr'));
@@ -37,19 +39,20 @@ function attachUpdateSelects(context) {
       var selectedFieldType = (selectedField in fields) ? fields[selectedField].type : null;
       var selectedFieldWidget = (selectedField in fields) ? fields[selectedField].widget : null
       var options = (selectedFieldType && (selectedFieldType in widgetTypes)) ? widgetTypes[selectedFieldType] : [ ];
-      this.targetSelect.contentOptions(options, selectedFieldWidget);
+      this.targetSelect.contentPopulateOptions(options, selectedFieldWidget);
 
       if (updateText) {
-        $(this.targetTextfield)
-          .attr('value', (selectedField in fields) ? fields[selectedField].label : '');
+        $(this.targetTextfield).attr('value', (selectedField in fields) ? fields[selectedField].label : '');
       }
     });
 
+    // Trigger change on initial pageload to get the right widget options
+    // and label when field type comes pre-selected (on failed validation).
     $(this).trigger('change', false);
   });
 }
 
-jQuery.fn.contentOptions = function(options, selected) {
+jQuery.fn.contentPopulateOptions = function(options, selected) {
   return this.each(function() {
     var disabled = false;
     if (options.length == 0) {
@@ -57,12 +60,17 @@ jQuery.fn.contentOptions = function(options, selected) {
       disabled = true;
     }
 
-    var selectValue = selected || this.options[this.selectedIndex].value;
+    // If possible, keep the same widget selected when changing field type.
+    // This is based on textual value, since the internal value might be
+    // different (optionwidgets_buttons vs. nodereference_buttons).
+    var previousSelectedText = this.options[this.selectedIndex].text;
 
     var html = '';
     jQuery.each(options, function(value, text) {
-      selected = (value == selectValue) ? ' selected="selected"' : '';
-      html += '<option value="' + value + '"' + selected +'>' + text + '</option>';
+      // Figure out which value should be selected. The 'selected' param
+      // takes precedence.
+      var is_selected = ((typeof(selected) !== 'undefined' && value == selected) || (typeof(selected) == 'undefined' && text == previousSelectedText));
+      html += '<option value="' + value + '"' + (is_selected ? ' selected="selected"' : '') +'>' + text + '</option>';
     });
 
     $(this)
